0efc1dab659f52092517f4ff4414c35d989987e2
DarshitBhuva <85541602+DarshitBhuva@users.noreply.github.com>
2021-10-15 14:06:54 +0530

    Add files via upload

---
+++ quizes/python-quiz.js
@@ -0,0 +1,130 @@
+const quizDB = [
+    {
+        question: "Q1: What is a correct syntax to output 'Hello World' in Python?",
+        a: 'echo "Hello World"',
+        b: 'print("Hello World)"',
+        c: 'echo("Hello World)"',
+        d: 'p("Hello World)"',
+        ans: "ans2"
+
+    },
+
+    {
+        question: "Q2: How do you insert COMMENTS in Python code?",
+        a: '//This is a comment',
+        b: '#This is a Comment',
+        c: '/*This is a Comment*/',
+        d: '**This is a Comment',
+        ans: 'ans2'
+
+    },
+
+    {
+        question: "Q3: Which one is NOT a legal variable name?",
+        a: "my-var",
+        b: "Myvar",
+        c: "my_var",
+        d: '_myvar',
+        ans: 'ans1'
+    },
+
+    {
+        question: "Q4: How Do You create a variable with numberic Value 5?",
+        a: "x = int(5)",
+        b: "Both the Other answers are correct",
+        c: "x=5",
+        d: "None Of Given Options",
+        ans: "ans2"
+    },
+
+    {
+        question: "Q5: What is the correct file extension for Python files?",
+        a: ".pt",
+        b: ".py",
+        c: ".pyth",
+        d: ".pyt",
+        ans: "ans2"
+    },
+
+    {
+        question: "Q6: What is the correct syntax to output the type of a variable or object in Python?",
+        a: "print(typeOf(x))",
+        b: "print(typeof(x))",
+        c: "print(tyof x)",
+        d: "print(type(x))",
+        ans: "ans4"
+    },
+    {
+        question: "Q7: What is the correct syntax to return the first character in a string?",
+        a: "function myFunction()",
+        b: "def myFunction()",
+        c: "create myFunction()",
+        d: "func myFunction()",
+        ans: "ans2"
+    },
+    {
+        question: "Q8: What is the correct way to create a function in Python?",
+        a: 'x = sub("Hello", 0, 1)',
+        b: 'x = "Hello"[0]',
+        c: 'x = "Hello".sub(0,1)',
+        d: "None of the given Options",
+        ans: "ans2"
+    },
+    {
+        question: "Q9: Which method can be used to remove any whitespace from both the beginning and the end of a string?",
+        a: 'ptrim()',
+        b: 'strip()',
+        c: 'trim()',
+        d: "len()",
+        ans: "ans3"
+    },
+    {
+        question: "Q10: Which method can be used to replace parts of a string?",
+        a: 'replaceString()',
+        b: 'switch()',
+        c: 'replace()',
+        d: "repl()",
+        ans: "ans3"
+    },
+    {
+        question: "Q11: Which of these collections defines a LIST?",
+        a: '{"apple","banana","cherry"}',
+        b: '["apple","banana","cherry"]',
+        c: '{"name":"apple","color":"green"}',
+        d: '("apple";"banana";"cherry")',
+        ans: "ans2"
+    },
+    {
+        question: "Q12: Which of these collections defines a tuple?",
+        a: '{"apple","banana","cherry"}',
+        b: '["apple","banana","cherry"]',
+        c: '{"name":"apple","color":"green"}',
+        d: '("apple";"banana";"cherry")',
+        ans: "ans4"
+    },
+    {
+        question: "Q13: Which of these collections defines a DICTIONARY?",
+        a: '{"apple","banana","cherry"}',
+        b: '["apple","banana","cherry"]',
+        c: '{"name":"apple","color":"green"}',
+        d: '("apple";"banana";"cherry")',
+        ans: "ans3"
+    },
+    {
+        question: "Q13: Which collection is ordered, changeable, and allows duplicate members?",
+        a: 'List',
+        b: 'Dictionary',
+        c: 'tuple',
+        d: 'sets',
+        ans: "ans1"
+    },
+    {
+        question: "Q14: Which collection doesn't allow duplicate members?",
+        a: 'List',
+        b: 'Dictionary',
+        c: 'tuple',
+        d: 'sets',
+        ans: "ans4"
+    },
+]
+
