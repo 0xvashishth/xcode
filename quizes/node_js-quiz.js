f8fa002c12143d43e72e9c042e7abaf696d973cf
DarshitBhuva <85541602+DarshitBhuva@users.noreply.github.com>
2021-10-18 21:17:13 +0530

    Add files via upload

---
+++ quizes/node_js-quiz.js
@@ -0,0 +1,163 @@
+const quizDB = [
+    {
+        question: "Q1: REPL Stands For",
+        a: 'Research Eval Program Learn',
+        b: 'Read Event Print Loop',
+        c: 'Read Earn Print Loop',
+        d: 'Read Eval Print Loop',
+        ans: "ans2"
+
+    },
+    {
+        question: "Q2: Which of the following command will show version of Node?",
+        a: "$npm --version",
+        b: "$node --version",
+        c: "$npm getVersion",
+        d: "$node getVersion",
+        ans: "ans2"
+    },
+    {
+        question: "Q3: Which of the following statement is correct?",
+        a: "js is Server Side Language.",
+        b: "js is the Client Side Language.",
+        c: "js is both Server Side and Client Side Language.",
+        d: "None of the above.",
+        ans : "ans1"
+    },
+    {
+        question: "Q4: Which of the following command is used to start a REPL session?",
+        a: "$ node",
+        b: "$ node repl",
+        c: "$ node start",
+        d: "$ node console",
+        ans : "ans1"
+    },
+    {
+        question: "Q5: In which language is Node.js written?",
+        a: "JavaScript",
+        b: "C",
+        c: "C++",
+        d: "All of the above",
+        ans : "ans4"
+    },
+    {
+        question: "Q6: Which of the following command is used to install the Node.js express module?",
+        a: "$ npm install express",
+        b: "$ node install express",
+        c: "$ install express",
+        d: "None of the above",
+        ans : "ans1"
+    },
+    {
+        question: "Q7: Which of the following command is used to install the Node.js express module?",
+        a: "$ npm install express",
+        b: "$ node install express",
+        c: "$ install express",
+        d: "None of the above",
+        ans : "ans1"
+    },
+    {
+        question: "Q8:  Which of the following extension is used to save the Node.js files?",
+        a: ".js",
+        b: ".node",
+        c: ".java",
+        d: ".txt",
+        ans : "ans1"
+    },
+    {
+        question: "Q9:  Which of the following module is not a built-in node module?",
+        a: "zlib",
+        b: "https",
+        c: "https",
+        d: "fsread",
+        ans : "ans4"
+    },
+    {
+        question: "Q10:  Which of the following code print the platform of operating system?",
+        a: "console.log('platform : ' + os.platform);",
+        b: "console.log('platform : ' + os.platform());",
+        c: "console.log('platform : ' + os.getPlatform());",
+        d: "None of the above.",
+        ans : "ans2"
+    },
+    {
+        question: "Q11:  Which of the following method is used to return the current working directory of the process?",
+        a: "cwd();",
+        b: "dwd();",
+        c: "pwd();",
+        d: "None of the above.",
+        ans : "ans1"
+    },
+    {
+        question: "Q12:  Which of the following is not a valid language for Node.js?",
+        a: "JavaScript",
+        b: "C",
+        c: "Java",
+        d: "C++",
+        ans : "ans3"
+    },
+    {
+        question: "Q13:  Which of the following shortcut command is used to kill a process in Node.js?",
+        a: "Ctrl + B",
+        b: "Ctrl + K",
+        c: "Ctrl + T",
+        d: "Ctrl + C",
+        ans : "ans4"
+    },
+    {
+        question: "Q14:  How many Node object methods are available?",
+        a: "21",
+        b: "18",
+        c: "19",
+        d: "20",
+        ans : "ans2"
+    },
+    {
+        question: "Q15:  Which of the following route parameter formats are valid?",
+        a: "/books/!:from-:to",
+        b: "/flights/:from-:to",
+        c: "/users/:userId/books/:bookId",
+        d: "None of the above",
+        ans : "ans1"
+    },
+    {
+        question: "Q16: Which of the following module is required to create a web server?",
+        a: "net module",
+        b: "http module",
+        c: "net module",
+        d: "url module",
+        ans : "ans1"
+    },
+    {
+        question: "Q17: Which of the following is the correct syntax to initiate the Node.js File?",
+        a: "filename.js",
+        b: "javascript filename.js",
+        c: "node filename.js",
+        d: "node filename",
+        ans : "ans3"
+    },
+    {
+        question: "Q18: Which of the following platforms does Node.js support?",
+        a: "Windows",
+        b: "Macintosh",
+        c: "Unix/Linux",
+        d: "All of the above.",
+        ans : "ans4"
+    },
+    {
+        question: "Q19: Which of the following method can append specified content to a file?",
+        a: "fs.appendFile()",
+        b: "fs.open()",
+        c: "fs.writeFile()",
+        d: "None of the above.",
+        ans : "ans1"
+    },
+    {
+        question: "Q20: Which of the following areas, Node.js, is not advised to be used?",
+        a: "Single Page Applications",
+        b: "JSON APIs based Applications",
+        c: "CPU intensive applications",
+        d: "Data-Intensive Real-time Applications (DIRT)",
+        ans : "ans3"
+    },
+]    
